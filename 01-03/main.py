7b2989de694a89ef652bf4f24ec5395364cd7446
Bernardo P S Souza <beportoss@gmail.com>
2021-03-02 10:39:04 -0300

    menu inicial
    menu inicial está ok

--- 01-03/main.py
+++ 01-03/main.py
@@ -12,12 +12,16 @@ linha = f'{cores["verde"]}-=' * 16 + f'{cores["sem_cor"]}'
 
 
 def main():
+    # ------------------------
     print(linha)
-    print(f'{cores["amarelo"]}Atividade de Tópicos do dia 01/03!{cores["sem_cor"]}')
-    print('Escolha a forma que quer realizar a operação:')
-    print('[1] - Digitando o número do dia da semana \n (retorna o nome do dia)')
-    print('[2] - Digitando o nome do dia da semana \n (retorna o número do dia)')
-    escolha = str(input(f'{cores["amarelo"]}Qual você escolhe?'))
+    print(f'{cores["vermelho"]}Atividade de Tópicos do dia 01/03!{cores["sem_cor"]}')
+    print(linha)
+    print(f'{cores["amarelo"]}Escolha a forma que quer realizar a operação:{cores["sem_cor"]}')
+    print(f'{cores["amarelo"]}[1]{cores["sem_cor"]} - Digitando o número do dia da semana \n (retorna o nome do dia)')
+    print(f'{cores["amarelo"]}[2]{cores["sem_cor"]} - Digitando o nome do dia da semana \n (retorna o número do dia)')
+    print(linha)
+    # ----- até aqui ok --------
+    escolha = str(input(f'{cores["amarelo"]}Qual você escolhe?')) # colorindo tudo
 
     while not (escolha == '1' or escolha == '2'):
         print(f'{cores["vermelho"]}ERRO!!! Digite de novo...{cores["sem_cor"]}')
