052f8d2be8fd40b2a0cd50bfe749aad34b96722f
Bernardo P S Souza <beportoss@gmail.com>
2021-03-15 22:15:41 -0300

    aula 15-03

---
+++ 15-03/ex01.py
@@ -0,0 +1,7 @@
+# 1° exercício do dia ...
+
+def main():
+    a = 'Universidade Feevale'
+    for i in a:
+        if (i in 'topicos em computalão')
+            print(i)
---
+++ 15-03/letras.py
@@ -0,0 +1,14 @@
+# contar quantas letras E tem em universidade Feevale
+
+def main():
+    a = 'Universidade Feevale'
+    e = 0
+    for i in a:
+        if i == 'e':
+            e += 1
+            print(e)
+
+if __name__ == "__main__":
+    main()
+
+
