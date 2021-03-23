a2d24d651ba8d4d4bc06914ecd71671ae5105b3e
Bernardo P S Souza <beportoss@gmail.com>
2021-03-23 11:33:01 -0300

    exemplo dado em aula

--- 22-03/pan_lambda.py
+++ 22-03/pan_lambda.py
@@ -1,18 +1,19 @@
+# exemplo dado
+
 def classifica(atletas):
-        # sorted => ordena pelo início. Ex.: 2, 3, 1 => 1,2,3
     ordenados = sorted(atletas, key=lambda x: x[1])
 
     return tuple(x[0] for x in ordenados[:3])
 
 def main():
-    resultado = classifica([
+    podio = classifica([
         ('John', 9.2),
         ('Timoshenko', 9.8),
         ('Usain', 8.9),
         ('Ze', 9.5),
     ])
 
-    print(resultado)
+    print(podio)
 
 if __name__ == '__main__':
     main()
