b49f856c8bce0df664bf148ab903a894ff6c46b8
BEp0 <beportoss@gmail.com>
2021-04-20 11:25:31 -0300

    exercício concluido

---
+++ 19-04/main.py
@@ -0,0 +1,38 @@
+def criarPHE():
+    
+    fonte = open('/home/souza/Documents/Programas-GitHub/Topicos-avancado/19-04/files-pdb/cadeiaA.pdb', 'r')
+    destino = open( '/home/souza/Documents/Programas-GitHub/Topicos-avancado/19-04/files-pdb/fenil.pdb', 'w' )
+    cont = 0
+    
+    for i in fonte:
+        if ( i[17:20] == 'PHE'):
+            if ( i[13:15] == 'CA'):
+                    cont += 1 # conta quantos foram encontrados
+                    destino.write(i)
+
+    print(f'FORAM ENCONTRADOS {cont}') # retorna no terminal quantos foram encontrados
+
+    destino.close()
+    fonte.close()
+    
+
+def obtemCadeiaA():
+    
+    fonte = open('/home/souza/Documents/Programas-GitHub/Topicos-avancado/19-04/files-pdb/7kms.pdb', 'r')
+    destino = open( '/home/souza/Documents/Programas-GitHub/Topicos-avancado/19-04/files-pdb/cadeiaA.pdb', 'w' )
+
+    for i in fonte:
+        if ( i[:4] == 'ATOM' ):
+            if ( i[21] == 'A'):
+                destino.write(i)
+    
+    destino.close()
+    fonte.close()
+
+def main():
+
+    obtemCadeiaA() 
+    criarPHE()
+
+if __name__ == '__main__':
+    main()
--- 19-04/test-folder/pdb-test.py
+++ 19-04/test-folder/pdb-test.py
@@ -12,12 +12,18 @@ as linhas onde tenho um carbono CA e o aminoacido é PHE
 def obtemCadeiaA():
     
     fonte = open('/home/souza/Documents/Programas-GitHub/Topicos-avancado/19-04/7kms.pdb', 'r')
-    destino = open( '/home/souza/Documents/Programas-GitHub/Topicos-avancado/19-04/fenil.pdb', 'w' )
+    destino = open( '/home/souza/Documents/Programas-GitHub/Topicos-avancado/19-04/test-folder/fenil-test.pdb', 'w' )
     
     for i in fonte:
         if ( i[:4] == 'ATOM' ):
             if ( i[21] == 'A'):
-                destino.write(i)
+                if ( i[17:20] == 'PHE'):
+                    if ( i[13:15] == 'CA'):
+                        destino.write(i)
+
+    destino.close()
+    fonte.close()
+    
 '''
 def criarphe():
     fonte = open('/home/souza/Documents/Programas-GitHub/Topicos-avancado/19-04/7kms.pdb', 'r')
@@ -34,8 +40,6 @@ def criarphe():
     fonte.close()
 '''
 
-    destino.close()
-    fonte.close()
 
 def main():
 
