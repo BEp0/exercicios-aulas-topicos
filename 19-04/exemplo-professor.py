59298afd00590df26d8863615fb5d122a2723484
BEp0 <beportoss@gmail.com>
2021-04-19 22:21:33 -0300

    não esta escrevendo

--- 19-04/exemplo-professor.py
+++ 19-04/exemplo-professor.py
@@ -15,26 +15,31 @@ def obtemCadeiaA():
     for i in fonte:
         if ( i[:4] == 'ATOM' ):
             if ( i[21] == 'A'):
-                destino.write(i)
+                if (i[17:21] == 'PHE'):
+                    if (i[13:16] == 'CA'):
+                        destino.write(i)
 
 
     destino.close()
     fonte.close()
-
+'''
 def criarphe():
-    fonte = open('/home/souza/Documents/Programas-GitHub/Topicos-avancado/19-04/7kmsA.pdb', 'r')
+    fonte = open('/home/souza/Documents/Programas-GitHub/Topicos-avancado/19-04/7kms.pdb', 'r')
     destino = open('/home/souza/Documents/Programas-GitHub/Topicos-avancado/19-04/fenil.pdb', 'w')
 
     for i in fonte:
-        if (i[17:21] == 'PHE' and i[13:16] == 'CA'):
-            destino.write(i)
+        if ( i[:4] == 'ATOM' ):
+            if ( i[21] == 'A'):
+                if (i[:21] == 'PHE'):
+                    if (i[:16] == 'CA'):
+                        destino.write(i)
 
     destino.close()
     fonte.close()
-
+'''
 def main():
     obtemCadeiaA()
-    criarphe()
+    #criarphe()
 
 
 if __name__ == "__main__":
--- 19-04/pdb.py
+++ 19-04/pdb.py
@@ -11,13 +11,15 @@ as linhas onde tenho um carbono CA e o aminoacido é PHE
 
 def obtemCadeiaA():
     
-    fonte = open( '7kms.pdb', 'r' ) #caminhos inventados, muda de acordo com a maquina
-    destino = open( '7kmsA.pdb', 'w' )
+    fonte = open('/home/souza/Documents/Programas-GitHub/Topicos-avancado/19-04/7kms.pdb', 'r')
+    destino = open( '/home/souza/Documents/Programas-GitHub/Topicos-avancado/19-04/fenil.pdb', 'w' )
     
     for i in fonte:
         if ( i[:4] == 'ATOM' ):
             if ( i[21] == 'A'):
-                destino.write(i)
+                if (i[17:21] == 'PHE'):
+                    if (i[13:16] == 'CA'):
+                        destino.write(i)
 
 
     destino.close()
