4299287f6e70c40676adf5f6eb733cd3522836a0
BEp0 <beportoss@gmail.com>
2021-04-20 10:45:27 -0300

    testes

---
+++ 19-04/test-folder/pdb-test.py
@@ -0,0 +1,47 @@
+'''
+abrir um 3° arquivo 
+
+abrir o destino
+
+percorrer o destino e bravar no arquivo fenil
+
+as linhas onde tenho um carbono CA e o aminoacido é PHE
+'''
+
+
+def obtemCadeiaA():
+    
+    fonte = open('/home/souza/Documents/Programas-GitHub/Topicos-avancado/19-04/7kms.pdb', 'r')
+    destino = open( '/home/souza/Documents/Programas-GitHub/Topicos-avancado/19-04/fenil.pdb', 'w' )
+    
+    for i in fonte:
+        if ( i[:4] == 'ATOM' ):
+            if ( i[21] == 'A'):
+                destino.write(i)
+'''
+def criarphe():
+    fonte = open('/home/souza/Documents/Programas-GitHub/Topicos-avancado/19-04/7kms.pdb', 'r')
+    destino = open('/home/souza/Documents/Programas-GitHub/Topicos-avancado/19-04/fenil.pdb', 'w')
+
+    for i in fonte:
+        if ( i[:4] == 'ATOM' ):
+            if ( i[21] == 'A'):
+                if (i[:21] == 'PHE'):
+                    if (i[:16] == 'CA'):
+                        destino.write(i)
+
+    destino.close()
+    fonte.close()
+'''
+
+    destino.close()
+    fonte.close()
+
+def main():
+
+    obtemCadeiaA() 
+    #criarphe()
+
+if __name__ == '__main__':
+    main()
+         
